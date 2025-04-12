ba9444df1fb5d13539adcff814d58440048e3de2
MadsSRasmussen <madsrasmussendev@gmail.com>
2025-04-12 17:01:42 +0200

    docs: update schema module docs

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,33 @@
 /**
- * This module contains tools for creating schemas.
+ * **Schema** is a serializeable shcema decleration library written in typescript.
+ * The developer experience of declaring a schema is inspirred by that of [Zod](https://zod.dev/).
+ *
+ * ### Example schema
+ * To declare a schema, simply import the abstract Schema class from the module:
+ *
+ * ```ts
+ * import { Schema as s } from "@msrass/schema";
+ *
+ * const objectSchema = s.object({
+ *     stringSchema: s.string(),
+ *     numberSchema: s.number().describe("Only cool numbers..."),
+ *     enumSchema: s.enum(["foo", "bar"]).optional(),
+ *     arraySchema: s.array(s.boolean()),
+ * });
+ * ```
+ *
+ * ### Serialization
+ *
+ * To serialize a schema a serializer must be imported from the `@msrass/schema/serialization` module.
+ *
+ * ```ts
+ * // ...continuation of snippet from Example schema section
+ * import { OpenAISerializer } from "@msrass/schema/serialization";
+ *
+ * const serializer = new OpenAISerializer();
+ * const serializedSchema = serializer.serialize(objectSchema);
+ * const schemaInstance = serializer.deserialize(serializedSchema);
+ * ```
  * @module
  */
 export * from "./schema.ts";
